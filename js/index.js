464ba4a7db96f0913012a3cc4a3858564b2fd7e5
Jacob Oakley <jacoboakley21@gmail.com>
2018-05-10 06:30:41 +0000

    moved results info from console to webpage

--- js/index.js
+++ js/index.js
@@ -7,55 +7,59 @@ const getKeys = () => {
     type: 'GET',
     url: 'https://swapi.co/api/',
     success: (response) => {
+      
       /* Makes an array from the Object returned by the API */
       let category = Object.keys(response);
-      /* Loop through the array and create an element for each key*/
+      
+      /* Alpahbitize keys */
+      category.sort();
+      
+      /* Loop through the array and create an item in the dropdown menu for each key */
       for(let i = 0; i < category.length; i++) {
         $('.dropdown-menu').append(`<a class='category dropdown-item' id='${category[i]}' data-url='https://swapi.co/api/${category[i]}'>${category[i]}</a>`);
       }
+      
     }
   });
 };
 
 
 const getResults = () => {
-  /* Attach event handler to the dropdown menu */    
+  
+  /* Listen for click events in the dropdown menu */    
   $('.nav-item').on('click', () => {
     
     let category = event.target.id;
-      
-    
     let url = event.target.getAttribute('data-url');
-  /* Clear content Html */
+    
+    /* Clear content Html */
     $('.content').html('');
-  /* Replace content Html with retrieved data*/  
+    
+    /* Replace content Html with retrieved data*/  
     $.ajax({
       type: 'GET',
       url: url,
       success: (response) => {
         let pages = (Math.ceil(response.count / 10));
-      
+
         for (let p = 1; p <= pages; p++) {
           $.ajax({
             type: 'GET',
             url: `${url}/?page=${p}`,
             success: (response) => {
-              for (let i = 0; i < response.results.length; i++) {
-                
-                url = response.results[i].url;
-                
+              
+              response.results.forEach((item) => {
                 if (category === 'films') {
-                  let title = response.results[i].title;
-                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${url}'>${title}</a>`);
+                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${item.url}'>${item.title}</a>`);
                 }
                 else {
-                  let name = response.results[i].name;
-                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${url}'>${name}</a>`);
+                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${item.url}'>${item.name}</a>`);
                 }
-              }
+              });
+              
             }
           });
-        }
+        }        
       }
     });
   });
@@ -64,14 +68,23 @@ const getResults = () => {
 const displayInfo = () => {
   $('.content').on('click', () => {
     
+    let category = event.target.id;
     let url = event.target.getAttribute('data-url');
     
     $.ajax({
       type: 'GET',
       url: url,
       success: (response) => {
-        console.log(response);
-        $('.info').append(`<p>${response.name}</p>`);
+        
+        let results = Object.keys(response);
+        let value = Object.values(response);
+        
+        $('.info').html('');
+        
+        for (let i = 0; i < Object.keys(response).length; i++) {
+          $('.info').append(`<div class='results text-center'><h3>${results[i]}</h3><p>${value[i]}</p></div>`);
+        }
+        
       },
     });
   });
