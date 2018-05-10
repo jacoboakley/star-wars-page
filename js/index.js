752f95fde9191353006be2d678909b9ae27c8448
Jacob Oakley <jacoboakley21@gmail.com>
2018-05-10 03:40:41 +0000

    Added data-url to elements for api call

--- js/index.js
+++ js/index.js
@@ -8,84 +8,72 @@ const getKeys = () => {
     url: 'https://swapi.co/api/',
     success: (response) => {
       /* Makes an array from the Object returned by the API */
-      var category = $.makeArray(Object.keys(response));
+      let category = Object.keys(response);
       /* Loop through the array and create an element for each key*/
-      for(var i = 0; i < category.length; i++) {
-        $('.dropdown-menu').append(`<a class='category dropdown-item' id='${category[i]}'>${category[i]}</a>`);
+      for(let i = 0; i < category.length; i++) {
+        $('.dropdown-menu').append(`<a class='category dropdown-item' id='${category[i]}' data-url='https://swapi.co/api/${category[i]}'>${category[i]}</a>`);
       }
-    },
-    error: () => {
-      $('.dropdown-menu').html('Cannot Load');
-    },
-    dataType: 'json',
+    }
   });
 };
 
 
 const getResults = () => {
   /* Attach event handler to the dropdown menu */    
-  $('.dropdown-toggle').on('click', () => {
-
-    var category = event.target.id;
+  $('.nav-item').on('click', () => {
+    
+    let category = event.target.id;
       
-    if (category !== 'dropdownMenuButton') {
-    /* Clear content Html */
-      $('.content').html('');
-    /* Replace content Html with retrieved data*/  
-      $.ajax({
-        type: 'GET',
-        url: `https://swapi.co/api/${category}`,
-        success: (response) => {
-          
-          var pages = (Math.ceil(response.count / 10));
-        
-          for (var p = 1; p <= pages; p++) {
-            $.ajax({
-              type: 'GET',
-              url: `https://swapi.co/api/${category}/?page=${p}`,
-              success: (response) => {
-                for (var i = 0; i < response.results.length; i++) {
-                  
-                  if (category === 'films') {
-                    var title = response.results[i].title;
-                    $('.content').append(`<a class='nav-link' id='${category}' title='${title}'>${title}</a>`);
-                  }
-                  else {
-                    var name = response.results[i].name;
-                    $('.content').append(`<a class='nav-link' id='${category}' title='${name}'>${name}</a>`);
-                  }
+    
+    let url = event.target.getAttribute('data-url');
+  /* Clear content Html */
+    $('.content').html('');
+  /* Replace content Html with retrieved data*/  
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: (response) => {
+        let pages = (Math.ceil(response.count / 10));
+      
+        for (let p = 1; p <= pages; p++) {
+          $.ajax({
+            type: 'GET',
+            url: `${url}/?page=${p}`,
+            success: (response) => {
+              for (let i = 0; i < response.results.length; i++) {
+                
+                url = response.results[i].url;
+                
+                if (category === 'films') {
+                  let title = response.results[i].title;
+                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${url}'>${title}</a>`);
+                }
+                else {
+                  let name = response.results[i].name;
+                  $('.content').append(`<a class='nav-link' id='${category}' data-url='${url}'>${name}</a>`);
                 }
               }
-            });
-          }
-        },
-        error: () => {
-          $('.content').text('Cannot Load');
-        },
-        dataType: 'json',
-      });
-    }
+            }
+          });
+        }
+      }
+    });
   });
 };
 
 const displayInfo = () => {
   $('.content').on('click', () => {
     
-    var category = event.target.id;
-    var name = event.target.title;
+    let url = event.target.getAttribute('data-url');
     
     $.ajax({
       type: 'GET',
-      url: `https://swapi.co/api/${category}/?search=${name}`,
+      url: url,
       success: (response) => {
-        console.log(response.results);
+        console.log(response);
+        $('.info').append(`<p>${response.name}</p>`);
       },
-      error: () => {
-        console.log('No Data Found');
-      }
     });
-    
-  
   });
 };
 
